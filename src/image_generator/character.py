90b04236eedceb0e310b35015e89545287a3d54b
Vaib Kapoor <vaib.kapoor15@gmail.com>
2019-04-27 19:14:58 -0400

    Moved character.py file, modifying paste_portrait to account for tuples, changed pink_color to have alpha=255 (I think it's alpha?)

---
+++ src/image_generator/character.py
@@ -0,0 +1,42 @@
+class Character:
+    def __init__(self, name, status, character_type):
+        self.name = name
+        self.status = status
+        self.character_type = character_type
+
+    def get_first_name(self):
+        first_name = self.name.split()[0]
+        return first_name.lower()
+
+    def get_status(self):
+        return self.status
+    
+    def get_character_type(self):
+        return self.character_type
+    
+    def get_character_path(self):
+        return "./assets/characters/formatted sprites/" + self.get_first_name() + "/" + self.get_status() + ".png"
+
+dr1_test = [
+    (
+        Character("Leon Kuwata", "dead", "killer"),
+        Character("Sayaka Maizono", "dead", "victim"),
+    ),
+    Character("Mukuro Ikusaba", "dead", "victim"),
+    (
+        Character("Mondo Oowada", "dead", "killer"),
+        Character("Chihiro Fujisaki", "dead", "victim"),
+    ),
+    (
+        Character("Celestia Ludenburg", "dead", "killer"),
+        Character("Hifumi Yamada", "dead", "victim"),
+        Character("Kiyotaka Ishimaru", "dead", "victim"),
+    ),
+    Character("Sakura Oogami", "dead", "victim"),
+    Character("Junko Enoshima", "dead", "victim"),
+    Character("Makoto Naegi", "alive", "spotless"),
+    Character("Kyoko Kirigiri", "alive", "spotless"),
+    Character("Toko Fukawa", "alive", "spotless"),
+    Character("Byakuya Togami", "alive", "spotless"),
+    Character("Asahina Aoi", "alive", "spotless"),
+]
--- src/image_generator/image_generator.py
+++ src/image_generator/image_generator.py
@@ -1,12 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-import os
-
-def create_character_list():
-    generator = os.walk("./assets/characters/formatted sprites")
-    unformatted = [x[0] for x in generator]
-    unformatted.pop(0) # this is the directory itself, not the subdirectories
-    formatted = [path.replace("\\", "/") for path in unformatted]
-    return formatted
+from character import Character, dr1_test
 
 def paste_portrait(bg, im, coords):
     bg.paste(im, coords, mask=im)
@@ -16,28 +9,48 @@ def paste_arrow(bg, coords):
     bg.paste(im, coords, mask=im)
 
 def paste_portraits(bg):
-    character_list = create_character_list()
+    characters = dr1_test
     initial_coords = (30, 413)
     coords = initial_coords
 
-    characters_length = len(character_list)
+    characters_length = len(characters)
     i = 0
+    portrait_number = 0
 
     while i < characters_length:
-        im = Image.open(character_list[i]+'/alive.png')
-        paste_portrait(bg, im, coords)
-        if(i == 7):
+        
+        if(isinstance(characters[i], tuple)):
+            j = 0
+            tuple_length = len(characters[i])
+            while j < tuple_length:
+
+                character = characters[i][j]
+                im = Image.open(character.get_character_path())
+                paste_portrait(bg, im, coords)
+
+                # if character.get_character_type == "killer":
+                    # paste_arrow(bg, coords)
+                
+                if(portrait_number == 7):
+                    coords = (initial_coords[0], initial_coords[1]+326)
+                else:
+                    coords = (coords[0]+238, coords[1])
+                j += 1
+                portrait_number += 1
+
+        if(portrait_number == 7):
             coords = (initial_coords[0], initial_coords[1]+326)
         else:
             coords = (coords[0]+238, coords[1])
         i += 1
+        portrait_number += 1
 
 def create_title(bg):
     draw = ImageDraw.Draw(bg)
     fancy_font = ImageFont.truetype("./assets/fonts/MrGrieves-Regular.otf", size=120)
     bg_width = 1920
     msg = "Killing Game Simulator"
-    pink_color = (255, 0, 240, 0)
+    pink_color = (255, 0, 240, 255)
     text_width = draw.textsize(msg, font=fancy_font)[0]
     coords = ((bg_width - text_width)/2, 28)
     draw.text(coords, msg, fill=pink_color, font=fancy_font, align="center")
@@ -46,7 +59,7 @@ def create_chapter_number(bg):
     draw = ImageDraw.Draw(bg)
     fancy_font = ImageFont.truetype("./assets/fonts/MrGrieves-Regular.otf", size=60)
     msg = "Prologue"
-    pink_color = (255, 0, 240, 0)
+    pink_color = (255, 0, 240, 255)
     coords = (27, 200)   
     draw.text(coords, msg, fill=pink_color, font=fancy_font, align="left")
 
